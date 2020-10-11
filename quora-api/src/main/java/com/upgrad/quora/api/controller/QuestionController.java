2cdaafdf4282f336b44578cd8dae69fb470c2097
divyankvijayvergiya <divyankvijay.96@gmail.com>
2020-10-11 14:35:58 +0530

    question entity and question controller class created

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -0,0 +1,11 @@
+package com.upgrad.quora.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class QuestionController {
+
+
+}
---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -0,0 +1,13 @@
+package com.upgrad.quora.service.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@SuppressWarnings("all")
+@Entity
+@Table(name = "question")
+public class QuestionEntity {
+
+    private String id;
+
+}
