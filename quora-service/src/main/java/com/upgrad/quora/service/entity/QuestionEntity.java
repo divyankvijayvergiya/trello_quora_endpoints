fba82cbca21925b16de972eba08062ab6c55103f
divyankvijayvergiya <divyankvijay.96@gmail.com>
2020-10-11 14:48:04 +0530

    question entity class implemented

--- quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -1,13 +1,80 @@
 package com.upgrad.quora.service.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.ZonedDateTime;
 
 @SuppressWarnings("all")
 @Entity
 @Table(name = "question")
 public class QuestionEntity {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
+    @Column(name = "uuid")
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "content")
+    @Size(max = 500)
+    @NotNull
+    private String content;
+
+
+    @Column(name = "date")
+    @NotNull
+    private ZonedDateTime date;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(ZonedDateTime date) {
+        this.date = date;
+    }
+
+    public UserEntity getUserEntity() {
+        return userEntity;
+    }
+
+    public void setUserEntity(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
 }
